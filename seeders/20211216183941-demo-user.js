60904eaab61dcdab335f1d294ff106c8a1b7b6f8
talihaarif13 <taliha.arif@invozone.com>
2021-12-16 23:44:11 +0500

    seeder added

---
+++ seeders/20211216183941-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('customers', [{
+      name: 'John1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: 'John2',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     return queryInterface.bulkDelete('customers', null, {});
+  }
+};
