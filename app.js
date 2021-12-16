03adf9df4e46a0b4b96879e37a4df0405a0ae50a
talihaarif13 <taliha.arif@invozone.com>
2021-12-16 17:52:06 +0500

    middleware

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 var bodyParser = require("body-parser");
 
+
+
 app.get('/', (req, res) => {
     res.send("hello world");
 });
@@ -13,8 +15,24 @@ app.post('/',function(req,res){
     var name = req.body.username;
     console.log(name);
     res.send(name);
- });
- 
+});
+
+//middleware
+function logOriginalUrl(req, res, next){
+    console.log('request url', req.originalUrl);
+    next();
+}
+function logMethod(req, res, next){
+    console.log('request type', req.method);
+    next();
+}
+var logStuff = [logOriginalUrl, logMethod];
+app.get('/user', logStuff, function(req, res, next){
+    res.send('user info');
+})
+
+
+
 
 app.listen(3000, () => {
     console.log("app listening on 3000");
