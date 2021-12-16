b47d8997b3a226dc286bcb41475abcccca7f60ed
talihaarif13 <taliha.arif@invozone.com>
2021-12-16 17:27:10 +0500

    body parser added

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express()
+var bodyParser = require("body-parser");
 
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/',function(req,res){
+    var name = req.body.username;
+    console.log(name);
+    res.send(name);
+ });
+ 
 
 app.listen(3000, () => {
     console.log("app listening on 3000");
